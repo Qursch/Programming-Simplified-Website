47330e88c15daaf51bec952828d2864ad6aedfe9
Hazim <hazimarafa80@gmail.com>
2021-12-16 21:27:47 -0800

    Update userCourse.schema.ts

--- apps/api/src/schemas/userCourse.schema.ts
+++ apps/api/src/schemas/userCourse.schema.ts
@@ -10,18 +10,18 @@ export type UserCourseDocument = UserCourse & Document;
 @Schema()
 export class UserCourse {
 	@Prop()
-	id: string;
+		id: string;	
 	@Prop()
-	name: string;
-	@Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'Course' })
-	ref: Course;
+		name: string;
+	@Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'Course'})
+		ref: Course;
 	@Prop({ type: [{ type: mongoose.Schema.Types.ObjectId }], ref: 'Lesson' })
-	// eslint-disable-next-line @typescript-eslint/no-explicit-any
-	lessons: Lesson[];
+		// eslint-disable-next-line @typescript-eslint/no-explicit-any
+		lessons: Lesson[];
 	@Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'Users' })
-	user: Partial<User>;
+		user: Partial<User>;
 	@Prop()
-	completed: boolean;
+		completed: boolean;
 }
 
 export const UserCourseSchema = SchemaFactory.createForClass(UserCourse);
