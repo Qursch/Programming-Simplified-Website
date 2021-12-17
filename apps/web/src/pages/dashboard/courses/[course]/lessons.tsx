2982c0e1b6a0d450bb1bf4964c10e2439e0da5b7
Hazim <hazimarafa80@gmail.com>
2021-12-17 13:40:29 -0800

    Update lessons.tsx

--- apps/web/src/pages/dashboard/courses/[course]/lessons.tsx
+++ apps/web/src/pages/dashboard/courses/[course]/lessons.tsx
@@ -1,25 +1,26 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
 export default function Lessons() {
+	location.href = location.href + "/1";
 	return null;
 }
 
 // check what lesson they are on then redirect them
-export function getServerSideProps({
-	req,
-	res,
-}: {
-	req: NextApiRequest;
-	res: NextApiResponse;
-}) {
-	const lesson = "1"; // TODO: get current lesson from db
+// export function getServerSideProps({
+// 	req,
+// 	res,
+// }: {
+// 	req: NextApiRequest;
+// 	res: NextApiResponse;
+// }) {
+// 	const lesson = "1"; // TODO: get current lesson from db
 
-	res.writeHead(302, {
-		Location: lesson,
-	});
-	res.end();
+// 	res.writeHead(302, {
+// 		Location: lesson,
+// 	});
+// 	res.end();
 
-	return {
-		props: {},
-	};
-}
+// 	return {
+// 		props: {},
+// 	};
+// }
