cd08680cad5252c377a9fd7e155f53e2394e6c02
Hazim <hazimarafa80@gmail.com>
2021-12-17 18:42:11 -0800

    Update enrolled.tsx

--- apps/web/src/pages/dashboard/enrolled.tsx
+++ apps/web/src/pages/dashboard/enrolled.tsx
@@ -7,6 +7,7 @@ import {
 	Divider,
 	Box,
 	HStack,
+	VStack,
 } from "@chakra-ui/react";
 import Button from "@components/button";
 import Layout from "@components/dashboard/layout";
@@ -21,6 +22,7 @@ export default function Enrolled() {
 
 	useEffect(() => {
 		getUserCourses().then(({ data }) => {
+			if (!data) return;
 			data.forEach((course) => {
 				let completedLessons = 0;
 				course.lessons.forEach((lesson) => {
@@ -42,85 +44,97 @@ export default function Enrolled() {
 	return (
 		<Layout>
 			<Center minH="100vh">
-				<Stack>
-					<Heading>Enrolled Courses</Heading>
-					<Text>Pick up where you left off.</Text>
-					<SimpleGrid columns={1}>
-						{userCourses.length &&
-							userCourses.map((userCourse) => {
-								console.log(userCourse);
-								const sections = [
-									<Heading size="md">
-										{userCourse.name}
-									</Heading>,
-									<Stat
-										label="Name"
-										value={
-											userCourse.lessons[
-												parseInt(
-													userCourse.currentLesson
-												)
-											].name
-										}
-									/>,
-									<Stat
-										label="Lesson"
-										value={userCourse.currentLesson}
-									/>,
+				<VStack>
+					{userCourses.length ? (
+						<>
+							<Heading>Enrolled Courses</Heading>
+							<Text>Pick up where you left off.</Text>
+							<SimpleGrid columns={1}>
+								{userCourses.map((userCourse) => {
+									console.log(userCourse);
+									const sections = [
+										<Heading size="md">
+											{userCourse.name}
+										</Heading>,
+										<Stat
+											label="Name"
+											value={
+												userCourse.lessons[
+													parseInt(
+														userCourse.currentLesson
+													)
+												].name
+											}
+										/>,
+										<Stat
+											label="Lesson"
+											value={userCourse.currentLesson}
+										/>,
 
-									<Stat
-										label="You've Spent"
-										value="<time> hrs"
-									/>,
-									<Stat
-										label="Lesson Progress"
-										value={
-											userCourse.lessons[
-												parseInt(
-													userCourse.currentLesson
-												)
-											].progress *
-												100 +
-											"%"
-										}
-									/>,
-									<Stat
-										label="Course Progress"
-										value={courseProgress * 100 + "%"}
-									/>,
-								];
-								return (
-									<Stack
-										key={userCourse.id}
-										bgImage={`linear-gradient(135deg,rgba(0, 0, 0, 0.8),rgba(0, 0, 0, 0.5)), url('/courses/${userCourse?.id}.png')`}
-										bgSize="cover"
-										bgPosition="center"
-										rounded={rounded}
-										shadow={shadow}
-										py="25px"
-										px="50px"
-									>
-										{sections.map((section) => (
-											<>
-												{section}
-												<Box py={0.25} w="100%">
-													<Divider />
-												</Box>
-											</>
-										))}
-										<Center>
-											<NextChakraLink
-												href={`/dashboard/courses/${userCourse.id}/lessons/${userCourse?.currentLesson}`}
-												pt={4}
-											>
-												<Button>Continue</Button>
-											</NextChakraLink>
-										</Center>
-									</Stack>
-								);
-							})}
-					</SimpleGrid>
-				</Stack>
+										<Stat
+											label="You've Spent"
+											value="<time> hrs"
+										/>,
+										<Stat
+											label="Lesson Progress"
+											value={
+												userCourse.lessons[
+													parseInt(
+														userCourse.currentLesson
+													)
+												].progress *
+													100 +
+												"%"
+											}
+										/>,
+										<Stat
+											label="Course Progress"
+											value={courseProgress * 100 + "%"}
+										/>,
+									];
+									return (
+										<Stack
+											key={userCourse.id}
+											bgImage={`linear-gradient(135deg,rgba(0, 0, 0, 0.8),rgba(0, 0, 0, 0.5)), url('/courses/${userCourse?.id}.png')`}
+											bgSize="cover"
+											bgPosition="center"
+											rounded={rounded}
+											shadow={shadow}
+											py="25px"
+											px="50px"
+										>
+											{sections.map((section) => (
+												<>
+													{section}
+													<Box py={0.25} w="100%">
+														<Divider />
+													</Box>
+												</>
+											))}
+											<Center>
+												<NextChakraLink
+													href={`/dashboard/courses/${userCourse.id}/lessons/${userCourse?.currentLesson}`}
+													pt={4}
+												>
+													<Button>Continue</Button>
+												</NextChakraLink>
+											</Center>
+										</Stack>
+									);
+								})}
+							</SimpleGrid>
+						</>
+					) : (
+						<>
+							<Heading textAlign="center">
+								You aren't enrolled in any course
+							</Heading>
+							<NextChakraLink href="/dashboard/courses">
+								<Button>Enroll in a course</Button>
+							</NextChakraLink>
+						</>
+					)}
+				</VStack>
 			</Center>
 		</Layout>
 	);
