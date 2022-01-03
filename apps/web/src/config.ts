0904fb9713e4b753ce88f25cdee041039fe698e1
Hazim Arafa <hazimarafa80@gmail.com>
2022-01-03 07:20:19 -0800

    Update config.ts

--- apps/web/src/config.ts
+++ apps/web/src/config.ts
@@ -21,7 +21,7 @@ const META = {
 };
 
 const API_URL = IS_PRODUCTION
-	? "https://e84bg-72-193-196-156.ngrok.io"
+	? "https://e84b-72-193-196-156.ngrok.io"
 	: "http://localhost:8081";
 
 export { GA_TRACKING_ID, IS_PRODUCTION, META, API_URL };
