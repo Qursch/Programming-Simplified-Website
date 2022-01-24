4599215fe10db8cf9d2f5389c702d3d937da57c4
Hazim Arafa <hazimarafa80@gmail.com>
2022-01-23 19:47:16 -0800

    Delete apps directory

--- apps/web/src/components/container.tsx
+++
@@ -1,9 +0,0 @@
-import { Center } from "@chakra-ui/react";
-
-export default function Container(props: any) {
-	return (
-		<Center as="section" {...props}>
-			{props.children}
-		</Center>
-	);
-}
--- apps/web/src/pages/404.tsx
+++
@@ -1,4 +0,0 @@
-import { Text } from "@chakra-ui/react";
-export default function FourZeroFour(): JSX.Element {
-	return <Text>404</Text>;
-}
